fb8f286501456372ab8484f0fb9092c29d24079d
Jacob Adams <tookmund@gmail.com>
2017-08-23 16:11:46 -0400

    Remove examples and add back README.Debian and the manpage

--- menu.ex
+++
@@ -1,2 +0,0 @@
-?package(wily):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="wily" command="/usr/bin/wily"
--- wily.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the wily package
-#
-0 4	* * *	root	[ -x /usr/bin/wily_maintenance ] && /usr/bin/wily_maintenance
